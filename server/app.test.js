b0f53ce17c1fbb4ee54ad93144973b998a0cba2e
Julien Caussade <julien.caussade@ikmail.com>
2025-07-11 16:21:21 +0200

    fix(app.test.js)

--- server/app.test.js
+++ server/app.test.js
@@ -1,29 +1,10 @@
 const request = require("supertest");
 const app = require("./app");
 
-// Setting a timeout for the server to start
-jest.setTimeout(30000);
-
 describe("GET /", () => {
-  let server;
-
-  // Before all tests, start the server
-  beforeAll((done) => {
-    server = app.listen(3000, () => {
-      done();
-    });
-  });
-
-  // After all the tests, close the server
-  afterAll((done) => {
-    server.close(done);
-  });
-
   it("responds with a welcome message", async () => {
-    const response = await request(app)
-      .get("/")
-      .expect("Content-Type", /text\/html/);
+    const response = await request(app).get("/").expect(200);
 
-    expect(response.text).toBe("Adishatz moùnde !"); // Check the response text
+    expect(response.text).toBe("Adishatz moùnde !");
   });
 });
